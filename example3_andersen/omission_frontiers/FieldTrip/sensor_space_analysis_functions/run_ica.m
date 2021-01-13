ebf1f2517447b44dae35dde40a04a02fffcc2f6e
matsvanes <mats.vanes@psych.ox.ac.uk>
2021-01-13 11:11:30 +0100

    remove old version

--- example3_andersen/omission_frontiers/FieldTrip/sensor_space_analysis_functions/run_ica.m
+++
@@ -1,7 +0,0 @@
-function [ica] = run_ica(cfg, input_variables)
-% This function identifies independent components
-%
-% cfg can contain anything that ft_componentanalysis recognizes
-
-ica = ft_componentanalysis(cfg, input_variables{1});
-ica = {ica}; %% return as cell
